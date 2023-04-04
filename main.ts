18c486e5f26775a5791eb8c8d9831eb7e26fd2e2
a <a@a.a>
2023-04-04 18:07:47 +0300

    update settings tab

--- main.ts
+++ main.ts
@@ -4,10 +4,12 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 
 interface MyPluginSettings {
 	mySetting: string;
+	patterns: [{source: string, replacement: string}];
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	mySetting: 'default',
+	patterns: [{source: '', replacement: ''}],
 }
 
 export default class MyPlugin extends Plugin {
@@ -17,7 +19,7 @@ export default class MyPlugin extends Plugin {
 		await this.loadSettings();
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new AutreplaceSettingTab(this.app, this));
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		//this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
@@ -36,7 +38,7 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
+class AutreplaceSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
 	constructor(app: App, plugin: MyPlugin) {
@@ -44,23 +46,67 @@ class SampleSettingTab extends PluginSettingTab {
 		this.plugin = plugin;
 	}
 
+	refresh(): void {
+		this.display();
+	}
+
 	display(): void {
 		const {containerEl} = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Autreplace settings.'});
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+		this.plugin.settings.patterns.forEach((pattern, index) => {
+			const data = { ...pattern };
+			const setting = this.makePatternRow(containerEl, `#${index}`, data)
+			.addButton((button) => {
+				button.setButtonText("Save").onClick(async (evt) => {
+					this.plugin.settings.patterns[index] = data;
 					await this.plugin.saveSettings();
-				}));
+					this.refresh();
+				})
+			}).addButton((button) => {
+				button.setButtonText("Remove").setClass("settings-delete-btn")
+				.onClick(async (evt) => {
+					this.plugin.settings.patterns.splice(index, 1);
+					await this.plugin.saveSettings();
+					this.refresh();
+				});
+			});
+		});
+		const data = {source: '', replacement: ''};
+		const setting = this.makePatternRow(containerEl, "New", data).addButton((button) => {
+			button.setButtonText("Add").onClick(async (evt) => {
+				if (!(data.source && data.replacement) || setting.controlEl.querySelector('.autoreplace-setting-error')) {
+					return;
+				}
+				this.plugin.settings.patterns.push(data);
+				await this.plugin.saveSettings();
+				this.refresh();
+			});
+		});
+	}
+
+	makePatternRow(containerEl: HTMLElement, label: string, data: {source: string, replacement: string}): Setting {
+		const rowClass = 'autoreplace-setting-section';
+		const setting = new Setting(containerEl).setClass(rowClass);
+
+		setting.setName(label);
+		setting.addText((text) => {
+			text.setValue(data.source)
+				.setPlaceholder("Input").onChange((value) => {
+					text.inputEl.removeClass('autoreplace-setting-error');
+					data.source = value;
+				});
+		});
+		setting.addText((text) => {
+			text.setValue(data.replacement)
+				.setPlaceholder("Replacement").onChange((value) => {
+					text.inputEl.removeClass('autoreplace-setting-error');
+					data.replacement = value;
+				});
+		});
+		return setting;
 	}
 }
